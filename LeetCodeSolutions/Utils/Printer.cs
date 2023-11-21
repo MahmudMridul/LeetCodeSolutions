e5b13b75b5a878c33e19d7f9598747545e4ef2df
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-21 13:30:38 +0600

    Add Printer

---
+++ LeetCodeSolutions/Utils/Printer.cs
@@ -0,0 +1,15 @@
+﻿
+namespace LeetCodeSolutions.Utils
+{
+    public static class Printer
+    {
+        public static void Print<T>(IEnumerable<T> list)
+        {
+            if(!list.Any()) { Console.WriteLine("List is empty!"); }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
