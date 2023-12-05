b399c456ededc0ae84374ed760294859d28aa410
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-12-05 08:14:58 +0600

    Add MathFunctions

---
+++ LeetCodeSolutions/Math/MathFunctions.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LeetCodeSolutions.Math
+{
+    public static class MathFunctions
+    {
+        public static int Gcd(int a , int b)
+        {
+            if (a == b) return 0;
+            return Gcd(b % a, a);
+        }
+    }
+
+}
