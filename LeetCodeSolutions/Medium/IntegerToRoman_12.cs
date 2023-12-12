d88fa22b1d95ab2cf97da2fed62fdb1c5432a7e0
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-12-12 18:21:44 +0600

    Add IntegerToRoman_12

---
+++ LeetCodeSolutions/Medium/IntegerToRoman_12.cs
@@ -0,0 +1,82 @@
+﻿
+using System.Text;
+
+namespace LeetCodeSolutions.Medium
+{
+    public class IntegerToRoman_12
+    {
+        private Dictionary<int, string> map = new Dictionary<int, string>()
+        {
+            { 1, "I" },
+            { 2, "II" },
+            { 3, "III" },
+            { 4, "IV" },
+            { 5, "V" },
+            { 6, "VI" },
+            { 7, "VII" },
+            { 8, "VIII" },
+            { 9, "IX" },
+            { 10, "X" },
+            { 40, "XL" },
+            { 50, "L" },
+            { 90, "XC" },
+            { 100, "C" },
+            { 400, "CD" },
+            { 500, "D" },
+            { 900, "CM" },
+            { 1000, "M" }
+        };
+
+        private int[] keys = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+
+        public string IntToRoman(int num)
+        {
+            Stack<string> stack = new Stack<string>();
+            if (map.ContainsKey(num)) { return map[num]; }
+
+            int mod = 10;
+            while(num > 0)
+            {
+                
+                int needToFind = num % mod;
+                if(map.ContainsKey(needToFind))
+                {
+                    stack.Push(map[needToFind]);
+                }
+                else
+                {
+                    string roman = FindRoman(needToFind);
+                    stack.Push(roman);
+                }
+                num -= needToFind;
+                mod *= 10;
+            }
+
+            return string.Join("", stack);
+        }
+
+        private string FindRoman(int needToFind)
+        {
+            StringBuilder sb = new StringBuilder();
+            while(needToFind > 0)
+            {
+                int n = LargestSmallerEqualToN(needToFind);
+                sb.Append(map[n]);
+                needToFind -= n;
+            }
+            return sb.ToString();
+        }
+
+        private int LargestSmallerEqualToN(int n)
+        {            
+            for(int i = 0; i < keys.Length; ++i)
+            {
+                if (keys[i] > n)
+                {
+                    return keys[i - 1];
+                }
+            }
+            return 1000;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/Medium/IntegerToRoman_12_Tests.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace LeetCodeSolutionsTests.Medium
+{
+    [TestFixture]
+    internal class IntegerToRoman_12_Tests
+    {
+        [TestCase(3, "III")]
+        [TestCase(58, "LVIII")]
+        [TestCase(1994, "MCMXCIV")]
+        [TestCase(3999, "MMMCMXCIX")]
+        public void IntToRoman_Tests(int input, string expected)
+        {
+            var obj = new IntegerToRoman_12();
+            string result = obj.IntToRoman(input);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
