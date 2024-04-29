2bbf25948efd73b8dab5bea4414a9ce4c40d00c6
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-04-29 08:46:29 +0600

    Added leetcode 2610

---
+++ LeetCodeSolutions/Medium/ConvertArrayTo2D_2610.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeSolutions.Medium
+{
+    public class ConvertArrayTo2D_2610
+    {
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            IList <IList<int>> twoD = new List<IList<int>>();
+            int[] count = GetCountArray(nums);
+            int total = nums.Length;
+            while(total > 0)
+            {
+                IList<int> list = new List<int>();
+                for(int  i = 1; i < count.Length; ++i)
+                {
+                    if (count[i] > 0)
+                    {
+                        list.Add(i);
+                        --count[i];
+                        --total;
+                    }
+                }
+                twoD.Add(list);
+            }
+            return twoD;
+        }
+
+        private int[] GetCountArray(int[] nums)
+        {
+            int[] count = new int[201];
+            for(int i = 0; i < nums.Length; ++i)
+            {
+                ++count[nums[i]];
+            }
+            return count;
+        }
+    }
+}
