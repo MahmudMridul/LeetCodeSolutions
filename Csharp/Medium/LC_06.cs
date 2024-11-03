862bb3226f9af4e8b146bbe9ca82e4b4b43cb3d3
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-03 14:34:38 +0600

    new: add solution of LC 06

--- Csharp/Medium/LC_06.cs
+++ Csharp/Medium/LC_06.cs
@@ -2,114 +2,43 @@
 
 namespace Csharp.Medium
 {
-    public class ZigzagConversion_6
+    public class LC_06
     {
         public string Convert(string s, int numRows)
         {
-            if (numRows == 1)
+            if (numRows == 1 || s.Length == numRows) return s;
+            List<string>[] matrix = new List<string>[numRows];
+
+            for(int i = 0; i < matrix.Length; ++i)
             {
-                return s;
+                matrix[i] = new List<string>();
             }
-            if (numRows == 2)
+
+            int pointer = 0;
+            bool goDown = true;
+            for(int i = 0; i < s.Length; ++i)
             {
-                return SpecialCase(s, s.Length);
+                char c = s[i];
+                matrix[pointer].Add("" + c);
+
+                if (pointer == numRows - 1) goDown = false;
+                if (pointer ==  0) goDown = true;
+
+                pointer = goDown ? pointer + 1 : pointer - 1; 
             }
-            int size = s.Length;
-            int numCols = GetNumberOfColumns(size, numRows);
-            Console.WriteLine($"ROWS: {numRows} COLS: {numCols}");
 
-            char[,] chars = Create2DArray(s, size, numRows, numCols);
-            Print(chars);
-
-
-            return "";
-        }
-
-        private string SpecialCase(string s, int size)
-        {
             StringBuilder sb = new StringBuilder();
-            for (int ch = 0; ch < size; ch += 2)
+
+            for (int i = 0; i < matrix.Length; ++i)
             {
-                sb.Append(s[ch]);
+                List<string> row = matrix[i];
+                for(int j = 0; j < row.Count; ++j)
+                {
+                    sb.Append(row[j]);
+                }
             }
-            for (int ch = 1; ch < size; ch += 2)
-            {
-                sb.Append(s[ch]);
-            }
+
             return sb.ToString();
-        }
-
-        private char[,] Create2DArray(string s, int size, int rows, int cols)
-        {
-            char[,] chars = new char[rows, cols];
-            int ch = 0;
-            int col = 0;
-            while (ch < size)
-            {
-                for (int r = 0; r < rows; ++r)
-                {
-                    //if (ch >= size) break;
-                    //chars[r, col] = s[ch];
-                    ++ch;
-                    Console.WriteLine($"ROW: {r} COL: {col}");
-                }
-                ++col;
-                for (int r = rows - 2; r >= 0; --r)
-                {
-                    //if (ch >= size) break;
-                    //chars[r, col] = s[ch];
-                    ++ch;
-                    //++col;
-
-
-
-
-                    Console.WriteLine($"ROW: {r} COL: {col}");
-                }
-            }
-            return chars;
-        }
-
-        private int GetNumberOfColumns(int size, int rows)
-        {
-            int cols = 0;
-            while (size > 0)
-            {
-                if (size - rows >= 0)
-                {
-                    ++cols;
-                    size -= rows;
-                }
-                else
-                {
-                    ++cols;
-                    break;
-                }
-
-                if (size - (rows - 2) >= 0)
-                {
-                    cols += rows - 2;
-                    size -= rows - 2;
-                }
-                else
-                {
-                    cols += size;
-                    break;
-                }
-            }
-            return cols;
-        }
-
-        private void Print(char[,] chars)
-        {
-            for (int r = 0; r < chars.GetLength(0); ++r)
-            {
-                for (int c = 0; c < chars.GetLength(1); ++c)
-                {
-                    Console.Write(chars[r, c] + " ");
-                }
-                Console.WriteLine();
-            }
         }
     }
 }
--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -6,8 +6,8 @@ namespace Csharp
     {
         public static void Main(string[] args)
         {
-            string result = new LC_05().LongestPalindrome("cbbd");
-            Console.WriteLine(result);
+            string result = new LC_06().Convert("PAYPALISHIRING", 14);
+            Console.WriteLine(result == "PAYPALISHIRING");
         }
     }
 }
