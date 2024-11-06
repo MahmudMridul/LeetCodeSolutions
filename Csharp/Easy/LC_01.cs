97a67ec1d6686dff82847df4956f3e8aad53bd71
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-06 13:43:08 +0600

    new: add solution for LC_01

---
+++ Csharp/Easy/LC_01.cs
@@ -0,0 +1,22 @@
+﻿namespace Csharp.Easy
+{
+    internal class LC_01
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> list = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; ++i) 
+            {
+                if (list.ContainsKey(nums[i]) && list.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { i, list[target - nums[i]] };                    
+                }
+                else
+                {
+                    list.Add(nums[i], i);
+                }
+            }
+            return new int[] { };
+        }
+    }
+}
