36d90b908a39499fbb064845ec7169cee5050fac
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-07 16:34:06 +0600

    new: add solution of LC 11

---
+++ Csharp/Medium/LC_11.cs
@@ -0,0 +1,31 @@
+﻿
+using Csharp.Easy;
+
+namespace Csharp.Medium
+{
+    public class LC_11
+    {
+        public int MaxArea(int[] height)
+        {
+            if (height.Length == 2) return Math.Min(height[0], height[1]);
+            
+            int maxArea = 0, left = 0, right = height.Length - 1;
+
+            while (left < right )
+            {
+                int area = (Math.Abs(left - right)) * (Math.Min(height[left], height[right]));
+                maxArea = Math.Max(maxArea, area);
+
+                if (height[left] < height[right])
+                {
+                    ++left;
+                }
+                else
+                {
+                    --right;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
