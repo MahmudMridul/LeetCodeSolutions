b423a2432d3977de81c6481d0c66a9a551414eb8
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-08 17:14:37 +0600

    new: add solution of LC 209

---
+++ Csharp/Medium/LC_209.cs
@@ -0,0 +1,25 @@
+﻿using System.Formats.Tar;
+
+namespace Csharp.Medium
+{
+    public class LC_209
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            int left = 0, right = 0, windowSum = 0, size = int.MaxValue;
+
+            while (right < nums.Length)
+            {
+                windowSum += nums[right];
+                while (windowSum >= target)
+                {
+                    size = Math.Min(size, right - left + 1);
+                    windowSum -= nums[left];
+                    ++left;
+                }
+                ++right;
+            }
+            return size == int.MaxValue ? 0 : size;
+        }
+    }
+}
