ea8c1151d5ae2fb22b272f46d7f6a9b42530475e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-09 14:50:22 +0600

    chore: add helper functions

---
+++ Csharp/Helpers/Helper.cs
@@ -0,0 +1,38 @@
+﻿namespace Csharp.Helpers
+{
+    public class Helper
+    {
+        public static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            if (collection == null || collection.Count() == 0)
+            {
+                Console.WriteLine("Empty!");
+                return;
+            }
+            foreach (var item in collection)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintNestedCollection<T>(IEnumerable<IEnumerable<T>> collection)
+        {
+            if (collection == null || collection.Count() == 0)
+            {
+                Console.WriteLine("Empty!");
+                return;
+            }
+            foreach (var innerCollection in collection)
+            {
+                foreach (var item in innerCollection)
+                {
+                    Console.Write($"{item} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
