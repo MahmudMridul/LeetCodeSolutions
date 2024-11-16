bf0f034f6f1b1611bdcbf90e14a6a3072467f805
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-16 23:36:59 +0600

    new: add solution of LC 53

---
+++ Csharp/Medium/LC_53.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Csharp.Medium
+{
+    public class LC_53
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSum = nums[0], maxEnding = 0;
+
+            for(int i = 0; i < nums.Length; ++i)
+            {
+                maxEnding = Math.Max(maxEnding + nums[i], nums[i]);
+                maxSum = Math.Max(maxEnding, maxSum);
+            }
+            return maxSum;
+        }
+    }
+}
