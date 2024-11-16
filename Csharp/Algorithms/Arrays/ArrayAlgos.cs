b607c6a5423189c54ca5ff8a6a650e7812e16bce
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-16 23:37:33 +0600

    new: add Kadanes algorithm

---
+++ Csharp/Algorithms/Arrays/ArrayAlgos.cs
@@ -0,0 +1,17 @@
+﻿namespace Csharp.Algorithms.Arrays
+{
+    public class ArrayAlgos
+    {
+        public int Kadane(int[] nums)
+        {
+            int maxSum = nums[0], maxEnding = 0;
+
+            for(int i = 0; i < nums.Length; ++i)
+            {
+                maxEnding = Math.Max(maxEnding + nums[i], nums[i]);
+                maxSum = Math.Max(maxSum, maxEnding);
+            }
+            return maxSum;
+        }
+    }
+}
