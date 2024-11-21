b2056fbf545e554fe5dac7d24d1aaf6b1851458f
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-21 13:20:11 +0600

    new: add solution of LC 219

---
+++ Csharp/Easy/LC_219.cs
@@ -0,0 +1,25 @@
+﻿namespace Csharp.Easy
+{
+    public class LC_219
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            HashSet<int> window = new HashSet<int>();
+            int left = 0;
+            for (int right = 0; right < nums.Length; ++right)
+            {
+                if (right - left > k)
+                {
+                    window.Remove(nums[left]);
+                    ++left;
+                }
+                if (window.Contains(nums[right]))
+                {
+                    return true;
+                }
+                window.Add(nums[right]);
+            }
+            return false;
+        }
+    }
+}
