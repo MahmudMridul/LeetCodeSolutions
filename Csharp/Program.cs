dde4ec071659fbf277a0529a4bfeb4a18c8b3597
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-25 13:14:56 +0600

    new: add solution of LC 03

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -10,10 +10,10 @@ namespace Csharp
         public static void Main(string[] args)
         {
             int[] tArray = { 11, 13, 17, 23, 29, 31, 7, 5, 2, 3 };
-            string tString = "2";
+            string tString = "pwwkew";
             int k = 3, threshold = 5;
 
-            int val = new LC_1343().NumOfSubarrays(tArray, k, threshold);
+            int val = new LC_03().LengthOfLongestSubstring(tString);
             Console.WriteLine(val);
         }
     }
