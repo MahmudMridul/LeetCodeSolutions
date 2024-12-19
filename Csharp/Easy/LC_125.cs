3ecc912de757d041fb0020422db48e75b57c4c45
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-19 08:02:45 +0600

    new: add solution of LC 125

---
+++ Csharp/Easy/LC_125.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Csharp.Easy
+{
+    public class LC_125
+    {
+        public bool IsPalindrome(string s)
+        {
+            s = s.ToLower();
+            string str = Regex.Replace(s, "[^a-zA-Z0-9]", "");
+
+            int left = 0, right = str.Length - 1;
+            while (left < right)
+            {
+                if (str[left] != str[right]) return false;
+                ++left; --right;
+            }
+            return true;
+        }
+    }
+}
