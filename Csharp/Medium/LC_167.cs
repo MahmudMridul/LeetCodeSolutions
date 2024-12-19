16a5ed9c02ee967f3e044ae8d78afb19a063e98c
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-19 08:03:11 +0600

    new: add solution of LC 167

---
+++ Csharp/Medium/LC_167.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Csharp.Medium
+{
+    public class LC_167
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int L = 0, R = numbers.Length - 1, sum = 0;
+            while (L < R)
+            {
+                sum = numbers[L] + numbers[R];
+                if (sum == target)
+                {
+                    return new int[] { L + 1, R + 1 };
+                }
+                else if (sum < target)
+                {
+                    ++L;
+                }
+                else
+                {
+                    --R;
+                }
+            }
+            return new int[] { };
+        }
+    }
+}
