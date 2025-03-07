2c79fdf1bb370e2e17a5cd46b92cf780e4b7540e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-03-07 07:58:36 +0600

    add: LC 232 #53

---
+++ Python/Easy/LC_232.py
@@ -0,0 +1,32 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+        self.front = None
+
+    def push(self, x: int) -> None:
+        if len(self.stack1) == 0:
+            self.front = x
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        if len(self.stack2) == 0:
+            while len(self.stack1) != 0:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if len(self.stack2) != 0:
+            return self.stack2[len(self.stack2) - 1]
+        return self.front
+
+    def empty(self) -> bool:
+        return len(self.stack1) == 0 and len(self.stack2) == 0
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
