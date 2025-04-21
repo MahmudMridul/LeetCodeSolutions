3a7c3f3cf6bb7336a19a7cda10cb8cf2377ffe43
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-04-21 17:23:12 +0600

    chore: move TreeNode to LC.Classes

---
+++ Csharp/LC.Classes/TreeNode.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Csharp.LC.Classes
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int v = 0, TreeNode l = null, TreeNode r = null)
+        {
+            val = v; left = l; right = r;
+        }
+    }
+}
