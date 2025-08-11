4874c145389aac20eb8ac21d4d09ba3b3afc9544
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-08-11 11:45:54 +0600

    Add LC 03

---
+++ Csharp/Patterns/LC00003_sliding_window_medium.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Csharp.Patterns.LC00003
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+
+            int longest = 1, left = 0, right = 1, size = 1;
+
+            int[] count = new int[256];
+            ++count[s[0]];
+
+            while (right < s.Length)
+            {
+                if (count[s[right]] > 0)
+                {
+                    --count[s[left]];
+                    ++left;
+                }
+                else
+                {
+                    ++count[s[right]];
+                    size = right - left + 1;
+                    longest = Math.Max(longest, size);
+                    ++right;
+                }
+            }
+            return longest;
+        }
+    }
+}
